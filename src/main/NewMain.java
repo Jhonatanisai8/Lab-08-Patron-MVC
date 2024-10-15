f942a25540513d5f3c62ec4b5799275fa0bfeffc
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-15 18:37:03 -0500

    clase ControladorEmpleado: modificacion metodos para los eventos en ambos botones y llamada e la clase main

--- src/main/NewMain.java
+++ src/main/NewMain.java
@@ -10,19 +10,4 @@ public class NewMain {
         frmRegistro de = new frmRegistro();
         ControladorEmpleado cd = new ControladorEmpleado(de);
     }
-
-    public static void ejemplo() {
-        System.out.println("HOLA MUNDO");
-        Empleado empleado = new Empleado();
-        empleado.setAñosExperiencia(5);
-        empleado.setCargo("Inspector de Calidad");
-        empleado.setCodigo("12312");
-        empleado.setNombres("Asociado de Almacén");
-        empleado.actualizarPagos(empleado);
-
-        System.out.println("Pago Bruto: " + empleado.getSueldoBruto());
-        System.out.println("Pago Adicional: " + empleado.getMontoAdicional());
-        System.out.println("Pago Final: " + empleado.getSueldoFinal());
-    }
-
 }
